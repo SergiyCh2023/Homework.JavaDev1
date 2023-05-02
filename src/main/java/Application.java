d3f4792f48fb5f3d986d8697194652eec21f0e38
SergiyCh2023 <alien13579@ukr.net>
2023-05-02 22:16:22 +0300

    Maven project

---
+++ src/main/java/Application.java
@@ -0,0 +1,21 @@
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+    User me = new User("Sergiy", "Chalapchiy");
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        String json = gson.toJson(me);
+
+
+        System.out.println(json);
+
+    }
+
+}
+
+
+
+
